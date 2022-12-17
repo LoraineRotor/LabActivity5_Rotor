bdb0121a84af4fab8f41b791835194d9c8794241
LoraineRotor <lkrotor2017@gmail.com>
2022-12-17 23:30:55 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -17,7 +17,36 @@ public class SimpleCalcGUI extends JFrame{
         app.setSize(420,420);
         app.setTitle("Simple Calculator");
         app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        app.cbOperations.addItem("+");
+        app.cbOperations.addItem("-");
+        app.cbOperations.addItem("*");
+        app.cbOperations.addItem("/");
     }
+    public SimpleCalcGUI(){
+        btnCompute.addActionListener(e -> {
+
+            int num1, num2, cons;
+
+            num1 = Integer.parseInt(tfNumber1.getText());
+            num2 = Integer.parseInt(tfNumber2.getText());
+
+            if (cbOperations.getSelectedItem().equals("+")) {
+                cons = (Integer.parseInt(tfNumber1.getText()) + Integer.parseInt(tfNumber2.getText()));
+                lblResult.setText(Integer.toString(cons));
+            } else if (cbOperations.getSelectedItem().equals("-")) {
+                cons = (Integer.parseInt(tfNumber1.getText()) - Integer.parseInt(tfNumber2.getText()));
+                lblResult.setText(Integer.toString(cons));
+            } else if (cbOperations.getSelectedItem().equals("*")) {
+                cons = (Integer.parseInt(tfNumber1.getText()) * Integer.parseInt(tfNumber2.getText()));
+                lblResult.setText(Integer.toString(cons));
+            } else if (cbOperations.getSelectedItem().equals("/")) {
+                cons = (Integer.parseInt(tfNumber1.getText()) / Integer.parseInt(tfNumber2.getText()));
+                lblResult.setText(Integer.toString(cons));
+            }
 
 
+
+            });
+        }
 }
